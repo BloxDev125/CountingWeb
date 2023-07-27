785252373a3fda8d24073171d63fc31949709ccd
BloxDev125 <101669877+BloxDev125@users.noreply.github.com>
2023-07-27 08:42:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+let count = 0;
+const clients = new Set();
+
+function broadcastCount() {
+  const message = JSON.stringify({ type: 'count', count });
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+app.use(express.static('public'));
+
+app.get('/count', (req, res) => {
+  res.json({ count });
+});
+
+app.post('/count/increment', (req, res) => {
+  count++;
+  broadcastCount();
+  saveCount();
+  res.json({ success: true });
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  const clientId = uuidv4();
+  clients.add(ws);
+
+  ws.send(JSON.stringify({ type: 'count', count }));
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
+
+function loadCount() {
+  if (fs.existsSync('count.json')) {
+    const data = fs.readFileSync('count.json', 'utf8');
+    count = JSON.parse(data).count;
+  }
+}
+
+function saveCount() {
+  fs.writeFileSync('count.json', JSON.stringify({ count }), 'utf8');
+}
+
+loadCount();
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,44 @@
+let count = 0;
+
+function updateCount() {
+  document.getElementById('count').innerText = count;
+}
+
+async function increment() {
+  try {
+    const response = await fetch('/count/increment', { method: 'POST' });
+    const data = await response.json();
+    if (!data.success) {
+      console.error('Error incrementing count on the server.');
+    }
+  } catch (error) {
+    console.error('Error incrementing count:', error);
+  }
+}
+
+function handleWebSocketMessage(event) {
+  const message = JSON.parse(event.data);
+  if (message.type === 'count') {
+    count = message.count;
+    updateCount();
+  }
+}
+
+document.getElementById('incrementButton').addEventListener('click', increment);
+
+const socket = new WebSocket(`ws://${location.host}`);
+socket.addEventListener('message', handleWebSocketMessage);
+
+function fetchCount() {
+  fetch('/count')
+    .then((response) => response.json())
+    .then((data) => {
+      count = data.count;
+      updateCount();
+    })
+    .catch((error) => {
+      console.error('Error fetching count:', error);
+    });
+}
+
+window.addEventListener('load', fetchCount);
